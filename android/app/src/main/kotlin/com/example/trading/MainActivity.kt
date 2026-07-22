dcb6eb6325c2aabf5849adf53f7a33a31e578c6d
Geektech <development.geektech@gmail.com>
2026-07-22 14:03:32 +0530

    Delete android directory

--- android/app/src/main/kotlin/com/example/trading/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tradingblitz.mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/tradingblitz/trading/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tradingblitz.trading
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
